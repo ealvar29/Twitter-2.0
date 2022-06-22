4cc06934f85a3693fea472fb0b4473e09cfbec7e
Eduardo <ealvarez@carminaticonsulting.com>
2022-06-22 08:49:45 -0500

    Adding sidebar icons

--- twitter-2/components/SidebarRow.tsx
+++ twitter-2/components/SidebarRow.tsx
@@ -7,9 +7,12 @@ interface Props {
 
 function SidebarRow({ Icon, title }: Props) {
   return (
-    <div>
-      <Icon />
-      <p>{title}</p>
+    <div
+      className="flex items-center space-x-2 px-4 py-3 rounded-full hover:bg-gray-100 cursor-pointer 
+    transitio duration-200 group max-w-fit"
+    >
+      <Icon className="h-6 w-6" />
+      <p className="group-hover:text-twitter">{title}</p>
     </div>
   );
 }
