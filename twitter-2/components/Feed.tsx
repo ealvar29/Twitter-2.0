e077e78a5e27d8ecf0306a0c1e912d693a2a496d
Eduardo <ealvarez@carminaticonsulting.com>
2022-06-23 07:11:18 -0500

    working on tweet box

--- twitter-2/components/Feed.tsx
+++ twitter-2/components/Feed.tsx
@@ -1,12 +1,19 @@
 import { RefreshIcon } from "@heroicons/react/outline";
 import React from "react";
+import Tweetbox from "./Tweetbox";
 
 function Feed() {
   return (
     <div className="col-span-7 lg:col-span-5">
       <div className="flex items-center justify-between">
         <h1 className="p-5 pb-0 text-xl font-bold">Home</h1>
-        <RefreshIcon className="h-8 w-8 text-twitter cursor-pointer mr-5 mt-5 transition-all duration-500 ease-out hover:rotate-180 active:scale-125" />
+        <RefreshIcon
+          className="h-8 w-8 text-twitter cursor-pointer mr-5 
+        mt-5 transition-all duration-500 ease-out hover:rotate-180 active:scale-125"
+        />
+      </div>
+      <div>
+        <Tweetbox />
       </div>
     </div>
   );
