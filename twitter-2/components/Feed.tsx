dcc62e848b9ee20f8872e5a50c474791a0fdeb68
Eduardo <ealvarez@carminaticonsulting.com>
2022-06-27 20:27:58 -0500

    mapping over tweets for middle column

--- twitter-2/components/Feed.tsx
+++ twitter-2/components/Feed.tsx
@@ -1,8 +1,14 @@
 import { RefreshIcon } from "@heroicons/react/outline";
 import React from "react";
+import { Tweet } from "../typings";
 import Tweetbox from "./Tweetbox";
+import TweetComponent from '../components/Tweet'
 
-function Feed() {
+interface Props {
+  tweets: Tweet[]
+}
+
+function Feed({ tweets }: Props) {
   return (
     <div className="col-span-7 lg:col-span-5">
       <div className="flex items-center justify-between">
@@ -14,6 +20,12 @@ function Feed() {
       </div>
       <div>
         <Tweetbox />
+
+        <div>
+          {tweets.map(tweet => (
+            <TweetComponent key={tweet._id} tweet={tweet}
+          ))}
+        </div>
       </div>
     </div>
   );
---
+++ twitter-2/components/Tweet.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Tweet } from "../typings";
+
+interface Props {
+  tweet: Tweet;
+}
+
+function Tweet(props: Props) {
+  return <div>Tweet</div>;
+}
+
+export default Tweet;
